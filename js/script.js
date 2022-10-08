c9771366376d9e10011f607dee95775d93932a19
J. Jackson <jjackson11211@gmail.com>
2022-10-08 12:40:50 -0400

    Show profile info

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const profileInfo = document.querySelector(".overview");
+const username = "jjackson112";
+
+const getData = async function () {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    displayData(data);
+};
+
+getData();
+
+const displayData = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.username} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.repos}</p>
+    </div>`
+
+    profileInfo.append(div);
+};
